52a0787b5f93e52b7f5bc0cc787c0378d992273e
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-12-06 00:55:00 -0800

    cleanup

--- Source/SheetRuler.js
+++ Source/SheetRuler.js
@@ -1,7 +1,7 @@
 /*
 ---
 name : SheetRuler
-description : SheetRuler will rock your socks!
+description : Low-level tool for manipulating styleSheets and cssRules
 
 authors   : Thomas Aylott
 copyright : © 2010 Thomas Aylott
@@ -16,26 +16,9 @@ var SheetRuler = {}
 var	styleSheets = document.styleSheets
 ,	styleSheetParent = document.documentElement
 
-SheetRuler.isSheetReal = isSheetReal
-SheetRuler.createStyleSheet = createStyleSheetWrapper
-SheetRuler.createRule = createStyleSheetRule
-SheetRuler.getElement = getElementForSheetOrRule
-
-/*
-SheetRuler.createStyleSheet = function(){
-	var sheet
-	sheet = createStyleSheet1()
-	if (sheet === styleSheets[styleSheets.length-1])
-		SheetRuler.createStyleSheet = createStyleSheet1
-	else sheet = createStyleSheet2()
-	if (sheet === styleSheets[styleSheets.length-1])
-		SheetRuler.createStyleSheet = createStyleSheet2
-	else throw new Error("This browser doesn't support creating styleSheets");
-	return sheet
-}
-*/
-
 // //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  // //
+
+SheetRuler.isSheetReal = isSheetReal
 function isSheetReal(sheet){
 	// tests if the sheet exists, has rules and is in the document.styleSheets collection
 	if (!(sheet && (sheet.cssRules || sheet.rules))) return false
@@ -43,6 +26,24 @@ function isSheetReal(sheet){
 	while (i--) if (styleSheets[i] === sheet) return true
 	return false
 }
+
+SheetRuler.getElement = getElementForSheetOrRule
+function getElementForSheetOrRule(sheetOrRule){
+	if (!sheetOrRule) return null
+	return	sheetOrRule.ownerNode
+		||	sheetOrRule.owningElement
+		||	SheetRuler.getElement(sheetOrRule.parentStyleSheet)
+}
+
+SheetRuler.createRule = createStyleSheetRule
+function createStyleSheetRule(sheet, selector, style){
+	var rules = sheet.cssRules || sheet.rules
+	if (sheet.insertRule) sheet.insertRule('' + selector + '{' + style + '}', rules.length)
+	else if (sheet.addRule) sheet.addRule(selector, style)
+	return rules[rules.length-1]
+}
+
+SheetRuler.createStyleSheet = createStyleSheetWrapper
 function createStyleSheetWrapper(name){
 	var	fallback
 	,	element
@@ -59,20 +60,6 @@ function createStyleSheetWrapper(name){
 		SheetRuler.createStyleSheet = createStyleSheet
 	return sheet
 }
-
-function getElementForSheetOrRule(sheetOrRule){
-	if (!sheetOrRule) return null
-	return	sheetOrRule.ownerNode
-		||	sheetOrRule.owningElement
-		||	SheetRuler.getElement(sheetOrRule.parentStyleSheet)
-}
-function createStyleSheetRule(sheet, selector, style){
-	var rules = sheet.cssRules || sheet.rules
-	if (sheet.insertRule) sheet.insertRule('' + selector + '{' + style + '}', rules.length)
-	else if (sheet.addRule) sheet.addRule(selector, style)
-	return rules[rules.length-1]
-}
-
 function createStyleSheet(name){
 	// Append a new STYLE element to the end of the greatest grandparent element.
 	//   This is the simplest way to ensure that the new STYLE is at the very end of the styleSheets collection.
@@ -112,56 +99,18 @@ function createStyleSheet(name){
 	styleSheetParent.appendChild(styleElement)
 	return styleElement.sheet || styleElement.styleSheet
 }
-// 
-// function createStyleSheet_fallback(name){
-// 	// Works in Safari 3.0.4
-// 	// Older versions of Safari strictly limited STYLE elements to children of the HEAD
-// 	// So, adding a STYLE element to a different parent dynamically fails silently.
-// 	var styleElement = document.createElement('style')
-// 	document.getElementsByTagName('head')[0].appendChild(styleElement)
-// 	return styleElement.sheet || styleElement.styleSheet
-// }
-
-function disableSheet(sheet){}
-function destroySheet(sheet){}
-function disableRule(rule){}
-function destroyRule(rule){}
-
-
-
-}(SheetRuler));
-
-
-// var styleParent = document.documentElement
-// var sheetCount = styleSheets.length
-// styleParent.appendChild(document.createElement("style"))
-// if (styleSheets.length == sheetCount)
-// 	styleParent = document.getElementsByTagName('head')[0]
-
-// document.getElementsByTagName('head')[0].appendChild(style = document.createElement("style"))
-
-// function createStyleSheet1_2(){
-// 	// Works in: Safari 5, Safari 3.0.4
-// 	// IE6 Operation Aborted
-// 	var	styles = document.getElementsByTagName('style')
-// 	,	lastSheetEl = styles[styles.length-1]
-// 	,	styleElement = document.createElement("style")
-// 	if (lastSheetEl) styleElement.parentNode.insertBefore(styleElement, lastSheetEl.nextSibling)
-// 	// else styleParent.appendChild(styleElement)
-// 	return styleElement.sheet || styleElement.styleSheet
-// }
-
-// function createStyleSheet1_1(){
-// 	var styleElement = document.createElement("link")
-// 	styleElement.setAttribute('ref', 'stylesheet')
-// 	styleElement.setAttribute('href', 'javascript:""')
-// 	document.documentElement.appendChild(styleElement)
-// 	var sheet = styleElement.sheet || styleElement.styleSheet
-// 	return sheet
-// }
 
 // //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  // //
+SheetRuler.disableSheet = disableSheet
+function disableSheet(sheet){}
 
-function String_escapeHTML(){
-	return ('' + this).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/"/g,'&quot;')
-}
+SheetRuler.destroySheet = destroySheet
+function destroySheet(sheet){}
+
+SheetRuler.disableRule = disableRule
+function disableRule(rule){}
+
+SheetRuler.destroyRule = destroyRule
+function destroyRule(rule){}
+
+}(SheetRuler));
